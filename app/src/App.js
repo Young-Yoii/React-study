ecf6e78914d5d15cf11b4bdd1863ce009f2e2964
gayoung jo <fhqht4545@gmail.com>
2022-11-18 14:14:59 +0900

    props

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+// import ReactPlayer from 'react-player'
 
-const Header = () => {
+const Header = ({title}) => {
   return <header>
-    <h1><a href="index.html">WEB</a></h1>
+    <h1><a href="index.html">{title}</a></h1>
   </header>
 }
 
@@ -16,19 +17,22 @@ const Nav = ()=> {
   </nav>
 }
 
-const Article = () => {
+// props를 쉽게? 알기 위해 구조분해할당으로 가져오기
+const Article = ({title, body}) => {
   return <article>
-    <h2>WELCOME</h2>
-    Hello, web
+    <h2>{title}</h2>
+    {body}
   </article>
 }
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header title="웹"/>
       <Nav />
-      <Article />
+      <Article title="어서오세요" body="헬로월드~!"/>
+      {/* <ReactPlayer url ="https://www.youtube.com/watch?v=kAPmMyEyl5k" width={500}/> 리액트 플레이어사용 */}
+      {/* 컴포넌트를 만들기 전에 사용자에게 어떤 props를 제공할지 UX를 먼저 설계, 그 후 구현하는 것이 좋은 방법 */}
     </div>
   );
 }
