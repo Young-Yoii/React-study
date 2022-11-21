69019a74a60d476283cfcd697c31f06090fe882b
gayoung jo <fhqht4545@gmail.com>
2022-11-21 20:24:58 +0900

    mode에 따라 UI가 달라지도록 구현

--- 1118/app/src/App.js
+++ 1118/app/src/App.js
@@ -38,17 +38,24 @@ const Article = ({title, body}) => {
 
 
 function App() {
+  let mode = 'WELCOME';
   const topics = [
     {id:1, title:"html", body:"html is..."},
     {id:2, title:"css", body:"css is..."},
     {id:3, title:"js", body:"js is..."},
   ]
+  let content = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello,Web!"/>
+  }else if (mode === 'READ'){
+    content = <Article title="Read" body="Hello,READ!"/>
+  }
   return (
     <div className="App">
       <Header title="Web" onChangeMode={() => {
         alert('WELCOME')}}/>
       <Nav topics={topics} onChangeMode={() => {alert('READ')}}/>
-      <Article title="Welcome" body="Hello,Web"/>
+      {content}
     </div>
   );
 }
