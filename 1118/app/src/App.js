9f5676f0c1c106d36b33cb1731eba55643b351c4
gayoung jo <fhqht4545@gmail.com>
2022-11-21 21:33:14 +0900

    글 선택 기능 구현

--- 1118/app/src/App.js
+++ 1118/app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { useState } from 'react';
 
 const Header = ({title , onChangeMode}) => {
   return <header>
     <h1><a href="index.html" onClick={(e) => {
       e.preventDefault()
-      onChangeMode()
+      onChangeMode('WELCOME')
     }}>{title}</a></h1>
   </header>;
 }
@@ -16,7 +17,7 @@ const Nav = ({topics , onChangeMode}) => {
       href={"/read/" + topic.id} 
       onClick={(e) => {
         e.preventDefault()
-        onChangeMode()
+        onChangeMode('READ', topic.id)
       }}
     >
       {topic.title}
@@ -38,23 +39,38 @@ const Article = ({title, body}) => {
 
 
 function App() {
-  let mode = 'WELCOME';
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
   const topics = [
     {id:1, title:"html", body:"html is..."},
     {id:2, title:"css", body:"css is..."},
     {id:3, title:"js", body:"js is..."},
   ]
+
+  const changeModeHandler = (mode, id) => {
+    setMode(mode);
+    if(id !== undefined){
+      setId(id)
+    }
+  }
   let content = null;
   if(mode === 'WELCOME'){
     content = <Article title="Welcome" body="Hello,Web!"/>
   }else if (mode === 'READ'){
-    content = <Article title="Read" body="Hello,READ!"/>
+    // let selected = null;
+    // for(let i = 0; i <topics.length; i++){
+    //   if(topics[i].id === id){
+    //     selected = topics[i];
+    //     break;
+    //   }
+    // }
+    const selected = topics.find(t => t.id === id)
+    content = <Article title={selected.title} body={selected.body}/>
   }
   return (
     <div className="App">
-      <Header title="Web" onChangeMode={() => {
-        alert('WELCOME')}}/>
-      <Nav topics={topics} onChangeMode={() => {alert('READ')}}/>
+      <Header title="Web" onChangeMode={changeModeHandler}/>
+      <Nav topics={topics} onChangeMode={changeModeHandler}/>
       {content}
     </div>
   );
