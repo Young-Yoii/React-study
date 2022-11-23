dea11a995905eb5fbb66f3b2e7c9413a2047cdb9
gayoung jo <fhqht4545@gmail.com>
2022-11-23 23:17:41 +0900

    Create 기능구현

--- 1122/app/src/App.js
+++ 1122/app/src/App.js
@@ -46,14 +46,30 @@ const Control = ({onChangeMode}) => {
   </ul>
 }
 
+const Create = ({onSave}) => {
+  const submitHandler = (e) => {
+    e.preventDefault()
+    const title = e.target.title.value;
+    const body = e.target.body.value;
+    onSave(title, body);
+  }
+
+  return <form onSubmit={submitHandler}>
+  <p><input type="text" name="title" placeholder='title' /></p>
+  <p><textarea name="body" placeholder='body'></textarea></p>
+  <p><input type="submit" value="Create" /></p>
+</form>
+}
+
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:"html", body:"html is..."},
     {id:2, title:"css", body:"css is..."},
     {id:3, title:"js", body:"js is..."},
-  ]
+  ])
 
   const changeModeHandler = (mode, id) => {
     setMode(mode);
@@ -61,6 +77,16 @@ function App() {
       setId(id)
     }
   }
+  const saveHandler = (title, body) => {
+    // add title and body to topics
+    let newTopics = [...topics];
+    newTopics.push({id:nextId, title, body})
+    setTopics(newTopics);
+    setId(nextId);
+    setNextId(current => current + 1);
+    setMode('READ');
+  }
+
   let content = null;
   if(mode === 'WELCOME'){
     content = <Article title="Welcome" body="Hello,Web!"/>
@@ -68,7 +94,7 @@ function App() {
     const selected = topics.find(t => t.id === id)
     content = <Article title={selected.title} body={selected.body}/>
   }else if(mode === 'CREATE') {
-    content = <Article title="CREATE" body="CREATE"/>
+    content = <Create onSave={saveHandler}/>
   }else if(mode === 'update') {
     content = <Article title="update" body="update"/>
   }
